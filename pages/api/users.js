2e8e6992e7014c6fb1d042d609fb8d978fe2c88c
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2025-06-02 17:54:42 -0300

    Hi guys, I started the setup schemas

---
+++ pages/api/users.js
@@ -0,0 +1,24 @@
+import dbConnect from '../../lib/mongodb';
+import User from '../../models/User';
+
+export default async function handler(req, res) {
+  await dbConnect();
+
+  if (req.method === 'POST') {
+    try {
+      const user = await User.create(req.body);
+      res.status(201).json({ success: true, data: user });
+    } catch (error) {
+      res.status(400).json({ success: false, error: error.message });
+    }
+  } else if (req.method === 'GET') {
+    try {
+      const users = await User.find();
+      res.status(200).json({ success: true, data: users });
+    } catch (error) {
+      res.status(400).json({ success: false, error: error.message });
+    }
+  } else {
+    res.status(405).json({ success: false, message: 'Not Allowed' });
+  }
+}
