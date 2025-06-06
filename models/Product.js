2a518f0b97973c33de747b2a3189927977dd795b
Mozart Soares <142760312+MozartSoares@users.noreply.github.com>
2025-06-06 00:12:33 -0300

    [BACKEND] Setup Auth and backend structure (#8)
    * refactor: setting up api
    
    - Update filies to typescript
    - Set up recommended mongo cache settings
    - Create types
    - Re-structure folders
    
    * feat: add auth
    
    -Add auth logic using nextauth
    -Setup backend main structure
    
    * fix: fix db connection

--- models/Product.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: String});
-
-export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
